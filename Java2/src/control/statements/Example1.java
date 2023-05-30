4e15d89d3b544a2f25ecdedf04fc3e4e00ec82c2
hdesale@rdalabs.com <hdesale@rdalabs.com>
2023-05-30 18:48:05 +0530

    Committing control statements to Branch

---
+++ Java2/src/control/statements/Example1.java
@@ -0,0 +1,19 @@
+package control.statements;
+
+public class Example1 
+{
+  public static void main(String[] args) {
+
+   String sName="Hitesh";
+   
+   if(sName.equals("Hitesh"))
+   {
+     System.out.println("Pass");
+   }
+   else
+   {
+     System.out.println("Fail");
+   }
+  }
+
+}
